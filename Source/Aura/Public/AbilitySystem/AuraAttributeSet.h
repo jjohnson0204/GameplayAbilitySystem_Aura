5acd2943e2893a5155392e5ba269d2a183ecdebc
jjohnson0204 <78010828+jjohnson0204@users.noreply.github.com>
2023-12-04 15:11:01 -0500

    Added heatlh and mana to AuraAttributeSet

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -13,5 +13,33 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+public:
+	UAuraAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	// Declare the variable
+	// Health
+	UPROPERTY(BlueprintReadOnly, Replicated = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health ;
+	UPROPERTY(BlueprintReadOnly, Replicated = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth ;
+
+	//Mana
+	UPROPERTY(BlueprintReadOnly, Replicated = OnRep_Mana, Category = "Vital Attributes")
+	FGameplayAttributeData Mana ;
+	UPROPERTY(BlueprintReadOnly, Replicated = OnRep_MaxMana, Category = "Vital Attributes")
+	FGameplayAttributeData MaxMana ;
+
+	// Create RepNotify that excepts the old value
+	//Health
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	//Mana
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 };
