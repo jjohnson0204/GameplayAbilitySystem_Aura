75132eee40621e80c97addfc129bc5f6fd398d9a
jjohnson0204 <78010828+jjohnson0204@users.noreply.github.com>
2023-12-07 14:34:54 -0500

    Added EffectApplied detection to AuraAbilitySystemComponent

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -3,3 +3,26 @@
 
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 
+void UAuraAbilitySystemComponent::AbilityActorInfoSet()
+{
+	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UAuraAbilitySystemComponent::EffectApplied);
+}
+
+void UAuraAbilitySystemComponent::EffectApplied(
+	UAbilitySystemComponent* AbilitySystemComponent,
+	const FGameplayEffectSpec& EffectSpec,
+	FActiveGameplayEffectHandle ActiveEffectHandle)
+{
+	//Broadcast the gameplay tag to the widget
+	//Tag container
+	FGameplayTagContainer TagContainer;
+	//Get tag
+	EffectSpec.GetAllAssetTags(TagContainer);
+	//Broadcast to widget
+	for (auto Tag : TagContainer)
+	{
+		//TODO: Broadcast the tag to the Widget Controller
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
+	}
+}
--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -24,4 +24,5 @@ public:
 	/** end Enemy Interface */
 protected:
 	virtual void BeginPlay() override;
+	virtual void InitAbilityActorInfo() override;
 };
