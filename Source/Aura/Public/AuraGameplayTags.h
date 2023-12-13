586983769cbadab5249d0818dfeb3bda239c463b
jjohnson0204 <78010828+jjohnson0204@users.noreply.github.com>
2023-12-13 15:40:49 -0500

    Added AuraAssetManager class and NativeTags for all attributes in AuraGameplayTags

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -15,12 +15,34 @@ struct FAuraGameplayTags
 {
 public:
 
- static const FAuraGameplayTags& Get() { return GameplayTags; }
+	static const FAuraGameplayTags& Get() { return GameplayTags; }
+	static void InitializeNativeGameplayTags();
 
- static void InitializeNativeGameplayTags();
+	// Primary
+	FGameplayTag Attributes_Primary_Strength;
+	FGameplayTag Attributes_Primary_Intelligence;
+	FGameplayTag Attributes_Primary_Resilience;
+	FGameplayTag Attributes_Primary_Vigor;
+	
+	// Secondary
+	FGameplayTag Attributes_Secondary_Armor;
+	FGameplayTag Attributes_Secondary_ArmorPenetration;
+	FGameplayTag Attributes_Secondary_BlockChance;
+	FGameplayTag Attributes_Secondary_CriticalHitChance;
+	FGameplayTag Attributes_Secondary_CriticalHitDamage;
+	FGameplayTag Attributes_Secondary_CriticalHitResistance;
+	FGameplayTag Attributes_Secondary_HealthRegeneration;
+	FGameplayTag Attributes_Secondary_ManaRegeneration;
+	FGameplayTag Attributes_Secondary_MaxHealth;
+	FGameplayTag Attributes_Secondary_MaxMana;
+
+	//Vital
+	FGameplayTag Attributes_Vital_Health;
+	FGameplayTag Attributes_Vital_Mana;
  
 protected:
 
 private:
- static FAuraGameplayTags GameplayTags;
+ 
+	static FAuraGameplayTags GameplayTags;
 };
