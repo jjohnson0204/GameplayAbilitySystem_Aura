2364094b620f38f2483b8cb9e8bac328c3ec0cd0
jjohnson0204 <78010828+jjohnson0204@users.noreply.github.com>
2023-12-13 14:03:40 -0500

    Added AuraGameplayTags struct to hold GameplayTags

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,16 @@
+// Copyright Light!!!!
+
+
+#include "AuraGameplayTags.h"
+
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.Armor"),
+		FString("Reduces damage taken, improves Block Chance")
+	);
+}
