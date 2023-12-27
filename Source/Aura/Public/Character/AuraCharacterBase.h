7a406d952f0a01a42ab87d6c5c64653c4788185b
jjohnson0204 <78010828+jjohnson0204@users.noreply.github.com>
2023-12-27 02:38:21 -0500

    Added GetLivePlayersWithinRadius function to AuraAbilitySystemLibrary

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -24,12 +24,16 @@ public:
 	
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
-	
-	virtual UAnimMontage* GetHitReactMontage_Implementation() override;
 
+	/** Combat Interface*/
+	virtual UAnimMontage* GetHitReactMontage_Implementation() override;
 	//Only on Server
 	virtual void Die() override;
-
+	virtual FVector GetCombatSocketLocation_Implementation() override;
+	virtual bool IsDead_Implementation() const override;
+	virtual AActor* GetAvatar_Implementation() override;
+	/** end Combat Interface*/
+	
 	UFUNCTION(NetMulticast, Reliable)
 	virtual void MultiCastHandleDeath();
 protected:
@@ -41,7 +45,7 @@ protected:
 	UPROPERTY(EditAnywhere, Category = "Combat")
 	FName WeaponTipSocketName;
 
-	virtual FVector GetCombatSocketLocation_Implementation() override;
+	bool bDead = false;
 	
 	UPROPERTY()
 	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
